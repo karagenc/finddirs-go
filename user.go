3fe76410c91f9a4f95338afcddbba12cb81a32f7
tomruk <41700170+tomruk@users.noreply.github.com>
2024-03-29 18:09:25 +0300

    Convert values of all fields of `UserDirs` to slash

--- user.go
+++ user.go
@@ -1,5 +1,7 @@
 package finddirs
 
+import "path/filepath"
+
 type UserDirs struct {
 	Desktop     string
 	Downloads   string
@@ -21,37 +23,57 @@ func RetrieveUserDirs() (userDirs *UserDirs, err error) {
 	if err != nil {
 		return
 	}
-	userDirs.Downloads, err = desktopDir()
+	userDirs.Desktop = filepath.ToSlash(userDirs.Desktop)
+
+	userDirs.Downloads, err = downloadsDir()
 	if err != nil {
 		return
 	}
+	userDirs.Downloads = filepath.ToSlash(userDirs.Downloads)
+
 	userDirs.Documents, err = documentsDir()
 	if err != nil {
 		return
 	}
+	userDirs.Documents = filepath.ToSlash(userDirs.Documents)
+
 	userDirs.Pictures, err = picturesDir()
 	if err != nil {
 		return
 	}
+	userDirs.Pictures = filepath.ToSlash(userDirs.Pictures)
+
 	userDirs.Videos, err = videosDir()
 	if err != nil {
 		return
 	}
+	userDirs.Videos = filepath.ToSlash(userDirs.Videos)
+
 	userDirs.Music, err = musicDir()
 	if err != nil {
 		return
 	}
+	userDirs.Music = filepath.ToSlash(userDirs.Music)
+
 	userDirs.Fonts, err = fontsDirs()
 	if err != nil {
 		return
 	}
+	for i, font := range userDirs.Fonts {
+		userDirs.Fonts[i] = filepath.ToSlash(font)
+	}
+
 	userDirs.Templates, err = templatesDir()
 	if err != nil {
 		return
 	}
+	userDirs.Templates = filepath.ToSlash(userDirs.Templates)
+
 	userDirs.PublicShare, err = publicShareDir()
 	if err != nil {
 		return
 	}
+	userDirs.PublicShare = filepath.ToSlash(userDirs.PublicShare)
+
 	return
 }
